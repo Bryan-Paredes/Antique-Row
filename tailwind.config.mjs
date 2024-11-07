9c1a8300c72c5d7f6bc275bed05500b90373a7a5
Bryan Paredes <bryansparedes2@gmail.com>
2024-11-07 14:02:48 -0600

    project settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "old-rose": {
+          50: "#faf6f6",
+          100: "#f6eeed",
+          200: "#eeddde",
+          300: "#e0c1c3",
+          400: "#ce9ea1",
+          500: "#b8777d",
+          600: "#a25c66",
+          700: "#864a54",
+          800: "#714049",
+          900: "#623943",
+          950: "#351c21",
+        },
+      },
+      backgroundImage: {
+        "bg-antique": "url(/bg-antique.webp)",
+        "bg-antique-2": "url(/bg-antique2.webp)",
+        "bg-bento1": "url(/bento/art.webp)",
+        "bg-bento2": "url(/bento/restaurant.webp)",
+        "bg-bento3": "url(/bento/antiquies.webp)",
+        "bg-bento4": "url(/bento/services.webp)",
+      },
+    },
+  },
+  plugins: [],
+};
